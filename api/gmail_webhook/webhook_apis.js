ea22a5bb6c303b2d7d1062cc3f72828d647742cd
Jimmy <j63guo@uwaterloo.ca>
2018-04-15 23:40:08 -0400

    asd

--- api/gmail_webhook/webhook_apis.js
+++ api/gmail_webhook/webhook_apis.js
@@ -1,3 +1,8 @@
+const config = require('../../src/config')
+
+const Twilio = require('twilio').Twilio;
+const client = new Twilio(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
+
 
 // incoming email from a known personal email, read what it says and reply as appropriate
 // dont just re-send the renthero redirect
@@ -11,8 +16,25 @@ exports.sendRentHeroRedirectEmail = function() {
 }
 
 // send the RentHero Redirect Email to a phone number
-exports.sendRentHeroRedirectSMS = function() {
-
+exports.sendRentHeroRedirectSMS = function(email) {
+  const p = new Promise((res, rej) => {
+    console.log(email)
+    client.messages.create({
+      body: 'Please visit www.renthero.ca',
+      to: '+15195726998',
+      from: '+17059996828',
+    })
+    .then((response) => {
+      console.log('=======sendRentHeroRedirectSMS RESPONSE========')
+      console.log(response)
+      res('Success')
+    })
+    .catch((err) => {
+      console.log(err)
+      res('Failed')
+    })
+  })
+  return p
 }
 
 // ask for personal email to send RentHero Redirect
